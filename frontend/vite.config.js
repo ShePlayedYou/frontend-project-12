46d42fbb9d19eb711766dfcf3edd61133f1982b8
Nastya <kota1213@mail.ru>
2025-06-20 23:46:04 +0300

    Updated vite config to fix CORS errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5002,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+      },
+      // Проксируем WebSocket соединения
+      '/socket.io': {
+        target: 'ws://localhost:5003',
+        ws: true,
+        rewriteWsOrigin: true,
+      },
+    },
+  },
+});
