046de74dc308eb0acd7d421e05891d1dac446e55
Nastya <kota1213@mail.ru>
2025-07-14 14:13:12 +0300

    Fixed unimported t

--- frontend/src/Hooks/useAuth.js
+++ frontend/src/Hooks/useAuth.js
@@ -1,19 +1,23 @@
 import { useDispatch } from 'react-redux'
 import { loginSuccess, logout } from '../slices/authSlice.js'
 import { handleReg, handleLogin } from '../API/api.js'
+import { useTranslation } from 'react-i18next'
 
 const useAuth = () => {
   const dispatch = useDispatch()
 
+  const { t } = useTranslation()
+
   const login = async (values) => {
-    const data = await handleLogin(values)
+    const data = await handleLogin(values, t)
     localStorage.setItem('token', data.token)
     localStorage.setItem('username', data.username)
     dispatch(loginSuccess(data))
   }
 
   const register = async (values) => {
-    const data = await handleReg(values)
+    const data = await handleReg(values, t)
+    console.log(data, 'FROM REGISTER useAUTH')
     localStorage.setItem('token', data.token)
     localStorage.setItem('username', data.username)
     dispatch(loginSuccess(data))
